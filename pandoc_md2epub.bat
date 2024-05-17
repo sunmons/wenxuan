50b55ef2e4a358e86c8f657a4c79bb827dab2693
sunmons <sun.mon@outlook.com>
2024-05-17 12:36:54 +0800

    make the bat

---
+++ pandoc_md2epub.bat
@@ -0,0 +1,4 @@
+pandoc -o ..\..\Documents\editted\文選\文選00.epub 00_title.txt _fanli.md 00.md --epub-chapter-level=2
+pandoc -o ..\..\Documents\editted\文選\文選01.epub 01_title.txt _fanli.md 01_1.md 01_2.md 01_3.md --epub-chapter-level=2
+pandoc -o ..\..\Documents\editted\文選\文選02.epub 02_title.txt _fanli.md 02.md
+pandoc -o ..\..\Documents\editted\文選\文選03.epub 03_title.txt _fanli.md 03.md
