6866ca5e70966e04cf573625df7df7ccd175462b
sunmons <sun.mon@outlook.com>
2024-05-28 11:37:19 +0800

    edit html output

--- pandoc_md2epub.bat
+++ pandoc_md2epub.bat
@@ -1,4 +1,5 @@
-pandoc -o ..\..\Documents\editted\文選\文選00.epub 00_title.txt _fanli.md 00.md --epub-chapter-level=2
-pandoc -o ..\..\Documents\editted\文選\文選01.epub 01_title.txt _fanli.md 01_1.md 01_2.md 01_3.md --epub-chapter-level=2
-pandoc -o ..\..\Documents\editted\文選\文選02.epub 02_title.txt _fanli.md 02.md
-pandoc -o ..\..\Documents\editted\文選\文選03.epub 03_title.txt _fanli.md 03.md
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan00.epub 00_title.txt _fanli.md 00.md --epub-chapter-level=2
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan01.epub 01_title.txt _fanli.md 01_1.md 01_2.md 01_3.md --epub-chapter-level=2
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan02.epub 02_title.txt _fanli.md 02.md
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan03.epub 03_title.txt _fanli.md 03.md
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan04.epub 04_title.txt _fanli.md 04_1.md 04_2.md 04_3.md --epub-chapter-level=2
