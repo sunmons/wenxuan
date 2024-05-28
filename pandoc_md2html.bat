aa173451dd31585260950b1431ef8b601e326299
sunmons <sun.mon@outlook.com>
2024-05-28 12:57:30 +0800

    add pv00.html

--- pandoc_md2html.bat
+++ pandoc_md2html.bat
@@ -1,5 +1,5 @@
-pandoc -o 00.html -c print.css -s 00.md
-pandoc -o 01.html -c print.css -s 01_1.md 01_2.md 01_3.md
-pandoc -o 02.html -c print.css -s 02.md
-pandoc -o 03.html -c print.css -s 03.md
-pandoc -o 04.html -c print.css -s 04_1.md 04_2.md 04_3.md
+pandoc -o pv00.html -c print.css -s 00.md
+pandoc -o pv01.html -c print.css -s 01_1.md 01_2.md 01_3.md
+pandoc -o pv02.html -c print.css -s 02.md
+pandoc -o pv03.html -c print.css -s 03.md
+pandoc -o pv04.html -c print.css -s 04_1.md 04_2.md 04_3.md
