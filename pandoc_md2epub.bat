05d83bc60b486592968f5df796de0352d497e6b5
sunmons <sun.mon@outlook.com>
2024-06-14 11:49:06 +0800

    update 05

--- pandoc_md2epub.bat
+++ pandoc_md2epub.bat
@@ -3,3 +3,4 @@ pandoc -o ..\..\Documents\editted\wenxuan\wenxuan01.epub 01_title.txt _fanli.md 
 pandoc -o ..\..\Documents\editted\wenxuan\wenxuan02.epub 02_title.txt _fanli.md 02.md
 pandoc -o ..\..\Documents\editted\wenxuan\wenxuan03.epub 03_title.txt _fanli.md 03.md
 pandoc -o ..\..\Documents\editted\wenxuan\wenxuan04.epub 04_title.txt _fanli.md 04_1.md 04_2.md 04_3.md --epub-chapter-level=2
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan05.epub 05_title.txt _fanli.md 05.md
--- pandoc_md2html.bat
+++ pandoc_md2html.bat
@@ -3,3 +3,4 @@ pandoc -o pv01.html -c print.css -s 01_1.md 01_2.md 01_3.md
 pandoc -o pv02.html -c print.css -s 02.md
 pandoc -o pv03.html -c print.css -s 03.md
 pandoc -o pv04.html -c print.css -s 04_1.md 04_2.md 04_3.md
+pandoc -o pv05.html -c print.css -s 05.md
