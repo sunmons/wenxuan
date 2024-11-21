65662eed2f5dc2743d1cdfdb43d34105196b7522
sunmons <sun.mon@outlook.com>
2024-11-21 15:12:17 +0800

    fixed some errors

--- pandoc_md2epub.bat
+++ pandoc_md2epub.bat
@@ -5,3 +5,5 @@ pandoc -o ..\..\Documents\editted\wenxuan\wenxuan03.epub 03_title.txt _fanli.md 
 pandoc -o ..\..\Documents\editted\wenxuan\wenxuan04.epub 04_title.txt _fanli.md 04_1.md 04_2.md 04_3.md --epub-chapter-level=2
 pandoc -o ..\..\Documents\editted\wenxuan\wenxuan05.epub 05_title.txt _fanli.md 05.md
 pandoc -o ..\..\Documents\editted\wenxuan\wenxuan06.epub 06_title.txt _fanli.md 06.md
+
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan.epub _title.txt _fanli.md 00.md 01_1.md 01_2.md 01_3.md 02.md 03.md 04_1.md 04_2.md 04_3.md 05.md 06.md 07_1.md 07_2.md 07_3.md 08_1.md 08_2.md 09.md 10.md 11.md 12.md 13.md 14.md 15.md 16.md 17.md 18.md 19.md
