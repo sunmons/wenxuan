050efdfcb859395dfeeafdded01788a37de64e66
Songming <sun.mon@outlook.com>
2025-03-26 19:43:55 +0800

    arranged total

--- pandoc_md2epub.bat
+++ pandoc_md2epub.bat
@@ -1,9 +1,1 @@
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan00.epub 00_title.txt _fanli.md 00.md --epub-chapter-level=2
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan01.epub 01_title.txt _fanli.md 01_1.md 01_2.md 01_3.md --epub-chapter-level=2
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan02.epub 02_title.txt _fanli.md 02.md
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan03.epub 03_title.txt _fanli.md 03.md
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan04.epub 04_title.txt _fanli.md 04_1.md 04_2.md 04_3.md --epub-chapter-level=2
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan05.epub 05_title.txt _fanli.md 05.md
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan06.epub 06_title.txt _fanli.md 06.md
-
-pandoc -o ..\..\Documents\editted\wenxuan\wenxuan.epub _title.txt _fanli.md 00.md 01_1.md 01_2.md 01_3.md 02.md 03.md 04_1.md 04_2.md 04_3.md 05.md 06.md 07_1.md 07_2.md 07_3.md 08_1.md 08_2.md 09.md 10.md 11.md 12.md 13.md 14.md 15.md 16.md 17.md 18.md 19.md
+pandoc -o ..\..\Documents\editted\wenxuan\wenxuan.epub _title.txt _fanli.md 00.md 01.md 2.md 3.md 4.md 5.md 6.md 7.md 8.md 9.md 10.md 11.md 12.md 13.md 14.md 15.md 16.md 17.md 18.md 19.md 20.md 21.md 22.md 23.md 24.md 25.md 26.md 27.md 28.md 29.md 30.md 31.md 32.md 33.md 34.md 35.md 36.md 37.md 38.md 39.md 40.md 41.md 42.md 43.md 44.md 45.md 46.md 47.md 48.md 49.md 50.md 51.md 52.md 53.md 54.md 55.md 56.md 57.md 58.md 59.md 60.md
